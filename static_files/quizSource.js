22b4edd134749fc58e7ce3d4eaec0390fc50c26c
virenabhyankar <forvirenra@gmail.com>
2019-10-25 01:21:54 -0700

    Added all functionality from quiz.js, fully integrated with server

--- static_files/quizSource.js
+++ static_files/quizSource.js
@@ -1,146 +1,139 @@
-$(document).ready(() => {
-    $('#testButton').click(() => {
-        const requestURL = '/questionQuery';
-        console.log('making ajax request to: ', requestURL);
-
-        $.ajax({
-            url: requestURL,
-            type: 'GET',
-            dataType: 'json',
-            success: (data) => {
-                console.log('You received some data!', data);
-
-                $('#testDiv').html(data[0]);
-                $('#hintDiv').html(data[1]);
-            }
-        })
-    });
-
-    $('#postButton').click(() => {
-        console.log('method initialized');
-        $.ajax({
-            url: 'postMsg',
-            type: 'POST',
-            data: {
-                questNum: pos+1
-            }
-        })
-    });
-});
-
-var pos = 0;
-var correct = 0;
-var totalAttempts = 0;
-var tryAgain = 0;
-var test, testStatus, hint, question, choice, correctness, options, optA, optB, optC, optD;
+var num = 1;
+var question = '';
+var choiceArray = [];;
 var alertBox;
-
-var questions = [
-    "How often do you play tennis?",
-    "Where do you usually eat lunch?",
-    "How long did you study last night?"
-]
-
-var answers = [
-    ["On Tuesday.", "For two hours.", "Almost every day.", "With John.", "C"],
-    ["Sandwich.", "With Jane.", "At 12:00.", "In the cafeteria.", "D"],
-    ["With Bob.", "In my room.", "English.", "For three hours.", "D"]
-]
-
-var hints = [
-    "This question is asking about frequency.",
-    "This question is asking about location.",
-    "This question is asking about time."
-]
-
+var correct, totalAttempts, tryAgain, numQuestions;
+correct = totalAttempts = tryAgain = numQuestions = 0;
 var wrong = [
     "Try again!",
     "Wrong!",
     "One more time!"
 ]
 
-// simplify commonly used function
-function get(x) {
-    return document.getElementById(x);
+function loadTest() { // Initiates quiz and removes button
+    var element = document.getElementById("init");
+    element.parentNode.removeChild(element);
+
+    $('#buttons').show();
+
+    var requestURL = '/numQuestions';
+
+    $.ajax({
+        url: requestURL,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            numQuestions = data[0]['COUNT(*)'];
+        }
+    })
+
+    loadData();
 }
 
-function askQuestion() {
-    test = get("test");
-    if (pos == questions.length) {
-        // end of test
-        test.innerHTML = "<h2>You completed the quiz in "+totalAttempts+" total attempts.</h2>";
-        get("testStatus").innerHTML = "Test completed";
-        pos = 0;
-        correct = 0;
+function loadData() {
+    $('#testStatus').html("Question "+num);
 
-        return false;
+    $.ajax({ // POST question number
+        url: 'postMsg',
+        type: 'POST',
+        data: {
+            questNum: num
+        }
+    })
+
+    var requestURL = '/questionOptions';
+
+    $.ajax({
+        url: requestURL,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            question = data[0].string;
+            for (var i = 0; i < data.length; i++) {
+                choiceArray.push(data[i].choiceID);
+            }
+            loadQuestion(question, choiceArray);
+        }
+    })
+}
+
+function loadQuestion(quest, choices) {
+    $('#question').html(quest);
+
+    var buttonOptions = document.getElementById('buttonOptions');
+    buttonOptions.innerHTML = '';
+    choiceArray = [];
+    for (var i = 0; i < choices.length; i++) {
+        choices[i]
+        buttonOptions.innerHTML += `<input type='radio' name='buttonOptions'>
+                                <label id='label${i}'>${choices[i]}</label><br>`;
     }
-
-    hint = get("hintBox");
-    hint.innerHTML = "";
-
-    get("testStatus").innerHTML = "Question "+(pos+1)+" of "+questions.length;
-    question = questions[pos];
-    optA = answers[pos][0];
-    optB = answers[pos][1];
-    optC = answers[pos][2];
-    optD = answers[pos][3];
-
-    test.innerHTML = "<h3>"+question+"</h3>";
-
-    test.innerHTML += "<input type='radio' name='options' value='A'> "+optA+"<br>";
-    test.innerHTML += "<input type='radio' name='options' value='B'> "+optB+"<br>";
-    test.innerHTML += "<input type='radio' name='options' value='C'> "+optC+"<br>";
-    test.innerHTML += "<input type='radio' name='options' value='D'> "+optD+"<br>";
-
-    test.innerHTML += "<br><span id='submit'><button onclick='checkAnswer()'>Submit</button></div>";
-    test.innerHTML += "<button onclick='giveHint()'>Hint</button>";
 }
 
-function giveHint() {
-    hint = get("hintBox");
-    hint.innerHTML = "<i>"+hints[pos]+"<br></br>";
+function getHint() {
+    var requestURL = '/hint';
+
+    $.ajax({
+        url: requestURL,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            $('#hintBox').html(data[0].hint);
+        }
+    })
 }
 
-function checkAnswer() {
-    options = document.getElementsByName("options");
-    for (var i=0; i<options.length; i++) {
-        if (options[i].checked) {
-            choice = options[i].value;
+function submit() {
+    var buttonOptions = document.getElementsByName('buttonOptions');
+    var userChoice = '';
+
+    for (var i = 0; i < buttonOptions.length; i++) {
+        if (buttonOptions[i].checked) {
+            userChoice = document.getElementById('label'+i).textContent;
+            break;
         }
     }
 
-    alertBox = get("alert-id");
+    var requestURL = '/submit';
 
-    if (choice==answers[pos][4]) {
-        correct++;
-        pos++;
+    $.ajax({
+        url: requestURL,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+            alertBox = document.getElementById("alert-id");
 
-        $(".alert").addClass("d-none");
-        alertBox.innerHTML = "";
+            var correctAnswer = data[0].choiceID;
+            if (userChoice == correctAnswer) { // CORRECT
+                num++;
+                correct++;
+                $(".alert").addClass("d-none");
+                alertBox.innerHTML = "";
 
-        alert("Correct!");
-        tryAgain = 0;
-    } else {
-        $(".alert").removeClass("d-none");
-        alertBox.innerHTML = "Sorry, that is incorrect. ";
+                alert("Correct!");
+                tryAgain = 0;
 
-        if (tryAgain > 0) {
-            alertBox.innerHTML += "<strong>"+wrong[tryAgain-1]+"</strong>";
+                if (num > numQuestions) {
+                    $('#test').html(`<h2>You completed the quiz in ${totalAttempts+1} total attempts.</h2>`);
+                    $('#testStatus').html("Test completed");
+                    num = 1;
+                    correct = 0;
+                } else {
+                    loadData();
+                };
+            } else { // INCORRECT
+                alertBox.innerHTML = "Sorry, that is incorrect. ";
+
+                if (tryAgain > 0) {
+                    alertBox.innerHTML += "<strong>"+wrong[tryAgain-1]+"</strong>";
+                    if (tryAgain == 3) {
+                        tryAgain = 0;
+                    }
+                }
+                tryAgain++;
+            }
+
+            totalAttempts++;
         }
-
-        if (tryAgain == 3) {
-            tryAgain = 0;
-        } else {
-            tryAgain++;
-        }
-    }
-
-    totalAttempts++;
-
-
-    askQuestion();
+    })
 }
-
-
-window.addEventListener("load", askQuestion, false);
